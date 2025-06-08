61dca81b87df48a6441aba467316c6ae5181b81f
Admin CastroJ <admcastroj@neuhausen.balluff.net>
2025-06-08 01:18:09 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Header } from "./components/Header"
+
+import styles from "./app.module.css"
+
+export function App() {
+
+  function handleRestartGame() {
+    alert("Reiniciar o jogo")
+  }
+
+  return (
+    <div className={styles.container}>
+      <main>
+        <Header current={5} max={10} onRestart={handleRestartGame} />
+      </main>
+    </div>
+  )
+}
+
