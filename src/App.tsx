158d9510b0e7ae30dadb74b9084de69c4d892283
Admin CastroJ <admcastroj@neuhausen.balluff.net>
2025-06-08 14:45:48 -0300

    Add letters used component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { Header } from "./components/Header"
 import { Tip } from "./components/Tip"
 import { Letter } from "./components/Letter"
+import { Input } from "./components/Input"
+import { Button } from "./components/Button"
+import { LettersUsed } from "./components/LettersUsed"
 
 import styles from "./app.module.css"
 
@@ -15,7 +18,24 @@ export function App() {
       <main>
         <Header current={5} max={10} onRestart={handleRestartGame} />
         <Tip tip="Linguagem de programação dinâmica" />
-        <Letter value="R" />
+
+        <div className={styles.word} >
+          <Letter value="R" />
+          <Letter value="E" />
+          <Letter value="A" />
+          <Letter value="C" />
+          <Letter value="T" />
+        </div>
+
+        <h4>Palpite</h4>
+
+        <div className={styles.guess}>
+          <Input autoFocus maxLength={1} placeholder="?" />
+          <Button title="Confirmar" />
+        </div>
+
+        <LettersUsed />
+
       </main>
     </div>
   )
